d43806e38a1f05556606a2aad7e823b84ae4a483
Max Czapanskiy <maxczap@stanford.edu>
2021-10-08 10:31:19 -0700

    Handle missing stickleback installation

--- R/init.R
+++ R/init.R
@@ -7,15 +7,20 @@
   # Retrieved 2021-10-05
   reticulate::configure_environment(pkgname)
 
-  # Import stickleback modules
-  .sbenv$sb <- reticulate::import("stickleback.stickleback")
-  .sbenv$sb_data <- reticulate::import("stickleback.data")
-  .sbenv$sb_util <- reticulate::import("stickleback.util")
-  .sbenv$sb_viz <- reticulate::import("stickleback.visualize", convert = FALSE)
+  # Check stickleback availability (Config/reticulate doesn't always work)
+  if (reticulate::py_module_available("stickleback")) {
+    # Import stickleback modules
+    .sbenv$sb <- reticulate::import("stickleback.stickleback")
+    .sbenv$sb_data <- reticulate::import("stickleback.data")
+    .sbenv$sb_util <- reticulate::import("stickleback.util")
+    .sbenv$sb_viz <- reticulate::import("stickleback.visualize", convert = FALSE)
 
-  # Import utility functions
-  util_path <- system.file("python", package = "rstickleback")
-  .sbenv$util <- reticulate::import_from_path("util",
-                                              util_path,
-                                              delay_load = TRUE)
+    # Import utility functions
+    util_path <- system.file("python", package = "rstickleback")
+    .sbenv$util <- reticulate::import_from_path("util",
+                                                util_path,
+                                                delay_load = TRUE)
+  } else {
+    message("Python package stickleback not found.")
+  }
 }
